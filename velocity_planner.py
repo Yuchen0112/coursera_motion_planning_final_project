Fixed sign error by acceleration

velocity_planner.py
@@ -236,7 +236,7 @@ class VelocityPlanner:
             for i in reversed(range(stop_index)):
                 dist = np.linalg.norm([path[0][i + 1] - path[0][i],
                                        path[1][i + 1] - path[1][i]])
-                vi = calc_final_speed(vf, -self._a_max, dist)
+                vi = calc_final_speed(vf, self._a_max, dist)
                 # We don't want to have points above the starting speed
                 # along our profile, so clamp to start_speed.
                 if vi > start_speed:
@@ -504,7 +504,7 @@ def calc_distance(v_i, v_f, a):
     # TODO: INSERT YOUR CODE BETWEEN THE DASHED LINES
     # ------------------------------------------------------------------
     d = (v_f ** 2 - v_i ** 2) / (2 * a)
-    return d if d > 0. else 0.
+    return d
     # ------------------------------------------------------------------
 
 